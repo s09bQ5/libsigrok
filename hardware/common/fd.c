57e9cd6a91c2e691bd38ed1b508fb928e54ec33a
Martin Ling <martin-git@earth.li>
2013-12-03 02:04:35 +0000

    Add debug output to fd blocking read code.

--- hardware/common/fd.c
+++ hardware/common/fd.c
@@ -23,6 +23,15 @@
 #include <unistd.h>
 #include <errno.h>
 
+/* Message logging helpers with subsystem-specific prefix string. */
+#define LOG_PREFIX "fd: "
+#define sr_log(l, s, args...) sr_log(l, LOG_PREFIX s, ## args)
+#define sr_spew(s, args...) sr_spew(LOG_PREFIX s, ## args)
+#define sr_dbg(s, args...) sr_dbg(LOG_PREFIX s, ## args)
+#define sr_info(s, args...) sr_info(LOG_PREFIX s, ## args)
+#define sr_warn(s, args...) sr_warn(LOG_PREFIX s, ## args)
+#define sr_err(s, args...) sr_err(LOG_PREFIX s, ## args)
+
 SR_PRIV int sr_fd_read_blocking(int fd, void *buf, size_t count, unsigned int timeout)
 {
 	size_t bytes_read = 0;
@@ -47,39 +56,55 @@ SR_PRIV int sr_fd_read_blocking(int fd, void *buf, size_t count, unsigned int ti
 		timeradd(&start, &delta, &end);
 	}
 
+	sr_spew("starting blocking read");
+
 	while (bytes_read < count)
 	{
 		FD_ZERO(&fds);
 		FD_SET(fd, &fds);
 		if (timeout) {
 			gettimeofday(&now, NULL);
-			if (timercmp(&now, &end, >))
+			if (timercmp(&now, &end, >)) {
+				sr_spew("timeout expired, read %d bytes", bytes_read);
 				return bytes_read;
+			}
 			timersub(&end, &now, &delta);
 		}
+		sr_spew("starting select()");
 		result = select(fd + 1, &fds, NULL, NULL, timeout ? &delta : NULL);
+		sr_spew("select() returned %d", result);
 		if (result < 0) {
-			if (errno == EINTR)
+			if (errno == EINTR) {
+				sr_spew("call interrupted");
 				continue;
-			else
+			} else {
+				sr_err("select() error: %m");
 				return SR_ERR;
+			}
 		} else if (result == 0) {
+			sr_spew("timeout in select, %d bytes read", bytes_read);
 			return bytes_read;
 		}
 
+		sr_spew("trying to read %d bytes", count - bytes_read);
 		result = read(fd, ptr, count - bytes_read);
 
 		if (result < 0) {
-			if (errno == EAGAIN)
+			if (errno == EAGAIN) {
+				sr_spew("no data");
 				continue;
-			else
+			} else {
+				sr_err("read() error: %m");
 				return SR_ERR;
+			}
 		}
 
+		sr_spew("read %d bytes", result);
 		bytes_read += result;
 		ptr += result;
 	}
 
+	sr_spew("complete, %d bytes read", bytes_read);
 	return bytes_read;
 }
 
