4c5fe199dec8213e56e862019fd09daae8becd23
Martin Ling <martin-git@earth.li>
2014-09-28 13:05:33 +0100

    Add a timeout parameter to blocking serial calls.
    Set this new parameter to 0 (no timeout) at every call site. This is
    consistent with previous behaviour, so cannot cause any regressions.
    
    Waiting forever for a serial operation is clearly always wrong. Without
    specific knowledge of each device and driver however, I can't choose
    appropriate timeouts for each call. The maintainers of these drivers
    will need to do so, and also add appropriate handling of timeouts.
    
    When this commit is merged, a bug should be entered for each driver
    that is touched by it.

--- src/hardware/agilent-dmm/sched.c
+++ src/hardware/agilent-dmm/sched.c
@@ -140,7 +140,7 @@ static int agdmm_send(const struct sr_dev_inst *sdi, const char *cmd)
 		strncat(buf, "\r\n", 32);
 	else
 		strncat(buf, "\n\r\n", 32);
-	if (serial_write_blocking(serial, buf, strlen(buf)) < strlen(buf)) {
+	if (serial_write_blocking(serial, buf, strlen(buf), 0) < strlen(buf)) {
 		sr_err("Failed to send.");
 		return SR_ERR;
 	}
